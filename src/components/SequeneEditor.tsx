395bd75099bc39971222f2dcb52b952dc649bf55
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-02-22 02:07:50 +0530

    feat: change layer sequence

--- src/components/SequeneEditor.tsx
+++ src/components/SequeneEditor.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
+import { KonvaEventObject } from "konva/lib/Node";
+import React, { LegacyRef, useEffect, useRef, useState } from "react";
 import {
   Stage,
   Layer,
@@ -87,6 +88,8 @@ const SequenceEditor = (props: Props) => {
 
   const [selectedId, selectShape] = React.useState("");
 
+  const [mouseOverSubLayerGroup, setMouseOverSubLayerGroup] = useState("");
+
   const checkDeselect = (e: { target: { getStage: () => any } }) => {
     // deselect when clicked on empty area
     const clickedOnEmpty = e.target === e.target.getStage();
@@ -95,7 +98,12 @@ const SequenceEditor = (props: Props) => {
     }
   };
 
-  const handleSequnceLayerDrag = () => {};
+  const handleSequnceLayerDrag = (id:any,newIndex:any) => {
+    const newSequenceData = [...sequenceData];
+    const index = newSequenceData.findIndex((step) => step.id === id);
+    newSequenceData[index].layerIndex = newIndex;
+    setSequenceData(newSequenceData);
+  };
 
   return (
     <>
@@ -109,6 +117,7 @@ const SequenceEditor = (props: Props) => {
         <Layer>
           {sequenceData.map((step, i) => (
             <SequenceLayer
+              key={i}
               step={step}
               i={i}
               windowDimensions={windowDimensions}
@@ -144,7 +153,8 @@ const SequenceEditor = (props: Props) => {
 };
 
 const roundnum = (num: number) => {
-  return Math.round(num / 50) * 50;
+  const index = Math.round(num / 50)
+  return index * 50;
 };
 
 const SequenceLayer = ({
@@ -158,35 +168,67 @@ const SequenceLayer = ({
   handleStepClick,
   handleSequnceLayerDrag,
 }: any) => {
-  const layerRef: any = React.useRef();
+  const layerRef:React.LegacyRef<any> = React.useRef();
   const subLayerGroup: any = React.useRef();
+
+  const [draggingSubLayer,setDraggingSubLayer] = useState<any>()
+
+  const [oldIndex,setOldIndex] = useState(i)
+
+  const onDragStart = (event: KonvaEventObject<DragEvent>, index: number) => {
+    event.target.setAttr('opacity', 0.5);
+    setOldIndex(index)
+    layerRef.current.moveToTop();
+    // event.evt.dataTransfer?.setData('text/plain', index.toString())
+    // event.dataTransfer.setData('text/plain', index);
+  };
+
+  const onDragOver = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+  };
+
+  const onDrop = (event:KonvaEventObject<DragEvent>) => {
+    event.target.setAttr('opacity', 1);
+    const draggedIndex = oldIndex+ (layerRef.current.attrs.y? layerRef.current.attrs.y/50:0)
+    console.log(oldIndex)
+    console.log(draggedIndex)
+    const newItems = [...sequenceData];
+    const [draggedItem] = newItems.splice(oldIndex, 1);
+    newItems.splice(parseInt(draggedIndex), 0, draggedItem);
+    console.log(newItems)
+    setSequenceData(newItems);
+  };
 
   return (
     <Group
       draggable
-      key={"step_" + step.id}
+      key={"step_" + step.id.toString()}
       ref={layerRef}
-      onDragStart={(evt) => {
-        layerRef.current.moveToTop();
-      }}
+      onDragStart={(event) =>{onDragStart(event, i);}}
+      onDragOver={onDragOver}
+      onDragEnd={(e) => onDrop(e)}
       dragBoundFunc={(pos) => {
-          return {
-            x: 0,
-            y: roundnum(pos.y),
-          }
+        return {
+          x: 0,
+          y: roundnum(pos.y),
+        };
       }}
     >
-      <Rect x={10} y={step.id * 50} width={100} height={40} fill="#4B4B4B" />
-      <Group>
+      <Rect x={10} y={i * 50} width={100} height={40} fill="#4B4B4B" />
+      <Group
+        ref={subLayerGroup}
+        id={"sub_layer_group"+step.id}
+      >
         <Rect
           x={10 + 101}
-          y={step.id * 50}
+          y={i * 50}
           width={windowDimensions.width - 120}
           height={40}
           fill="#4B4B4B"
         />
         <SubLayer
           id={step.id}
+          i={i}
           shapeProps={step}
           isSelected={step.id.toString() === selectedId}
           onSelect={() => {
@@ -197,11 +239,12 @@ const SequenceLayer = ({
             rects[i] = newAttrs;
             setSequenceData(rects);
           }}
+          setDraggingSubLayer={setDraggingSubLayer}
         />
       </Group>
       <Text
         x={35}
-        y={step.id * 50 + 20}
+        y={i * 50 + 20}
         text={step.text}
         fontSize={20}
         fill="white"
@@ -211,7 +254,15 @@ const SequenceLayer = ({
   );
 };
 
-const SubLayer = ({ id, shapeProps, isSelected, onSelect, onChange }: any) => {
+const SubLayer = ({
+  id,
+  i,
+  shapeProps,
+  isSelected,
+  onSelect,
+  onChange,
+  setDraggingSubLayer
+}: any) => {
   const shapeRef: any = React.useRef();
   const trRef: any = React.useRef();
 
@@ -223,11 +274,19 @@ const SubLayer = ({ id, shapeProps, isSelected, onSelect, onChange }: any) => {
     }
   }, [isSelected]);
 
+  // const handleGroupChange = () => {
+  //   console.log(mouseOverSequenceLayer);
+  //   if (mouseOverSequenceLayer) {
+  //     shapeRef.current.moveTo(mouseOverSequenceLayer.current);
+  //   }
+  // };
+
   return (
     <React.Fragment>
       <Rect
+      id={"sub_layer_"+id}
         x={10 + 101}
-        y={id * 50}
+        y={i * 50}
         width={120}
         height={40}
         fill="#BDBDBD"
@@ -237,6 +296,9 @@ const SubLayer = ({ id, shapeProps, isSelected, onSelect, onChange }: any) => {
             x: pos.x,
             y: roundnum(pos.y),
           };
+        }}
+        onDragStart={()=>{
+          setDraggingSubLayer(shapeRef)
         }}
         onClick={onSelect}
         onTap={onSelect}
--- src/views/Test.tsx
+++ src/views/Test.tsx
@@ -1,131 +1,46 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Stage, Layer, Rect, Transformer } from 'react-konva';
+import { DragEvent, useState } from 'react';
 
-const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }:any) => {
-  const shapeRef:any = React.useRef();
-  const trRef:any = React.useRef();
+function SortableList() {
+  const [items, setItems] = useState([
+    { id: 'item-1', content: 'Item 1' },
+    { id: 'item-2', content: 'Item 2' },
+    { id: 'item-3', content: 'Item 3' },
+    { id: 'item-4', content: 'Item 4' },
+  ]);
 
-  React.useEffect(() => {
-    if (isSelected) {
-      // we need to attach transformer manually
-      trRef.current.nodes([shapeRef.current]);
-      trRef.current.getLayer().batchDraw();
-    }
-  }, [isSelected]);
+  const onDragStart = (event: any, index: number) => {
+    event.dataTransfer.setData('text/plain', index);
+  };
 
-  return (
-    <React.Fragment>
-      <Rect
-        onClick={onSelect}
-        onTap={onSelect}
-        ref={shapeRef}
-        {...shapeProps}
-        draggable
-        onDragEnd={(e) => {
-          onChange({
-            ...shapeProps,
-            x: e.target.x(),
-            y: e.target.y(),
-          });
-        }}
-        onTransformEnd={(e) => {
-          // transformer is changing scale of the node
-          // and NOT its width or height
-          // but in the store we have only width and height
-          // to match the data better we will reset scale on transform end
-          const node = shapeRef.current;
-          const scaleX = node.scaleX();
-          const scaleY = node.scaleY();
+  const onDragOver = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+  };
 
-          // we will reset it back
-          node.scaleX(1);
-          node.scaleY(1);
-          onChange({
-            ...shapeProps,
-            x: node.x(),
-            y: node.y(),
-            // set minimal value
-            width: Math.max(5, node.width() * scaleX),
-            height: Math.max(node.height() * scaleY),
-          });
-        }}
-      />
-      {isSelected && (
-        <Transformer
-          ref={trRef}
-          boundBoxFunc={(oldBox, newBox) => {
-            // limit resize
-            if (newBox.width < 5 || newBox.height < 5) {
-              return oldBox;
-            }
-            return newBox;
-          }}
-        />
-      )}
-    </React.Fragment>
-  );
-};
-
-const initialRectangles = [
-  {
-    x: 10,
-    y: 10,
-    width: 100,
-    height: 100,
-    fill: 'red',
-    id: 'rect1',
-  },
-  {
-    x: 150,
-    y: 150,
-    width: 100,
-    height: 100,
-    fill: 'green',
-    id: 'rect2',
-  },
-];
-
-const Demo = () => {
-  const [rectangles, setRectangles] = React.useState(initialRectangles);
-  const [selectedId, selectShape] = React.useState('');
-
-  const checkDeselect = (e: { target: { getStage: () => any; }; }) => {
-    // deselect when clicked on empty area
-    const clickedOnEmpty = e.target === e.target.getStage();
-    if (clickedOnEmpty) {
-      selectShape('');
-    }
+  const onDrop = (event: any, index: number) => {
+    const draggedIndex = event.dataTransfer.getData('text');
+    console.log(draggedIndex)
+    console.log(index)
+    const newItems = [...items];
+    const [draggedItem] = newItems.splice(draggedIndex, 1);
+    newItems.splice(index, 0, draggedItem);
+    setItems(newItems);
   };
 
   return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onMouseDown={checkDeselect}
-      onTouchStart={checkDeselect}
-    >
-      <Layer>
-        {rectangles.map((rect, i) => {
-          return (
-            <Rectangle
-              key={i}
-              shapeProps={rect}
-              isSelected={rect.id === selectedId}
-              onSelect={() => {
-                selectShape(rect.id);
-              }}
-              onChange={(newAttrs: { x: number; y: number; width: number; height: number; fill: string; id: string; }) => {
-                const rects = rectangles.slice();
-                rects[i] = newAttrs;
-                setRectangles(rects);
-              }}
-            />
-          );
-        })}
-      </Layer>
-    </Stage>
+    <ul>
+      {items.map(({ id, content }, index) => (
+        <li
+          key={id}
+          draggable
+          onDragStart={(event) => onDragStart(event, index)}
+          onDragOver={onDragOver}
+          onDrop={(event) => onDrop(event, index)}
+        >
+          {content}
+        </li>
+      ))}
+    </ul>
   );
-};
+}
 
-export default Demo
+export default SortableList
