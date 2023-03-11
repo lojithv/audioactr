fed268fdf0f13a81525c83055b7162b6359bd192
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-03-11 12:49:46 +0530

    MUI theme change

--- src/theme/mui-theme.ts
+++ src/theme/mui-theme.ts
@@ -1,15 +1,15 @@
 import { createTheme } from '@mui/material/styles';
-import { grey } from '@mui/material/colors';
+import { blue, grey } from '@mui/material/colors';
 
 const theme = createTheme({
   palette: {
     mode:'dark',
     primary: {
-      main: '#3A3A3A',
+      main: blue[600],
       contrastText: "#fff"
     },
     secondary: {
-      main: grey[400],
+      main: blue[400],
     },
     text:{
       primary:'#fff'
