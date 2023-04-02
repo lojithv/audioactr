450a650f0d3206a84e9e7a3e3f6d9c72b2437a4b
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-04-02 12:13:30 +0530

    mark disabled phrase blocks

--- src/core/components/Phrase.tsx
+++ src/core/components/Phrase.tsx
@@ -1,55 +1,88 @@
 import { KonvaEventObject } from "konva/lib/Node";
-import React from "react";
+import React, { useState } from "react";
 import { Group, Rect, Text } from "react-konva";
-import { setContextMenuState, setEditorState, setSelectedPhrase, useEditorState, useSelectedPhrase } from "../../store/EditorStore";
+import {
+  setContextMenuState,
+  setEditorState,
+  setSelectedPhrase,
+  useEditorState,
+  useSelectedPhrase,
+} from "../../store/EditorStore";
 
 type Props = {};
 
-const Phrase = ({ id, trackIndex, layerData,phraseIndex }: any) => {
+const Phrase = ({ id, trackIndex, layerData, phraseIndex }: any) => {
   const shapeRef: any = React.useRef();
   const trRef: any = React.useRef();
 
   const grpRef: any = React.useRef();
 
-  const selectedPhrase = useSelectedPhrase()
+  const selectedPhrase = useSelectedPhrase();
 
-  const handleClick = (e:KonvaEventObject<MouseEvent>) => {
-    if(e.evt.button == 2){
-      setContextMenuState({open:true,event:e})
+  const handleClick = (e: KonvaEventObject<MouseEvent>) => {
+    if (e.evt.button == 2) {
+      setContextMenuState({ open: true, event: e });
     } else {
-      setContextMenuState({open:false,event:null})
+      setContextMenuState({ open: false, event: null });
     }
-    if(phraseData){
-      setSelectedPhrase(phraseData)
+    if (phraseData) {
+      setSelectedPhrase(phraseData);
     } else {
-      setSelectedPhrase(null)
+      setSelectedPhrase(null);
     }
-  }
-  
-  const editorState = useEditorState()
+  };
+
+  const editorState = useEditorState();
 
   const getPhrase = () => {
-   return editorState.phrases.find((p)=> p.trackIndex === trackIndex && p.phraseIndex === phraseIndex)
-  }
+    return editorState.phrases.find(
+      (p) => p.trackIndex === trackIndex && p.phraseIndex === phraseIndex
+    );
+  };
 
-  const phraseData = getPhrase()
+  const phraseData = getPhrase();
 
-  const handleDbClick = (e:KonvaEventObject<MouseEvent>) =>{
-    if(e.evt.button == 0){
-      setEditorState({...editorState, phrases: [...editorState.phrases,{trackId:trackIndex,trackIndex:trackIndex,phraseIndex:phraseIndex,phrase:'add phrase',id:editorState.phrases.length+1}]})
+  const handleDbClick = (e: KonvaEventObject<MouseEvent>) => {
+    if (e.evt.button == 0) {
+      const existingphrase = editorState.phrases.find(
+        (p) => p.phraseIndex === phraseIndex
+      );
+      if (!existingphrase) {
+        setEditorState({
+          ...editorState,
+          phrases: [
+            ...editorState.phrases,
+            {
+              trackId: trackIndex,
+              trackIndex: trackIndex,
+              phraseIndex: phraseIndex,
+              phrase: "add phrase",
+              id: editorState.phrases.length + 1,
+            },
+          ],
+        });
+      } else {
+      }
     }
-  }
+  };
+
+  const addRestriction = () => {
+    const existingphrase = editorState.phrases.find(
+      (p) => p.phraseIndex === phraseIndex
+    );
+    return existingphrase ? true : false;
+  };
 
   return (
     <Group
-      x={10 + 101 + 100*phraseIndex}
+      x={10 + 101 + 100 * phraseIndex}
       y={trackIndex * 50}
       // draggable
       width={100}
       height={40}
       ref={grpRef}
-      onClick={(e)=>handleClick(e)}
-      onDblClick={(e)=>handleDbClick(e)}
+      onClick={(e) => handleClick(e)}
+      onDblClick={(e) => handleDbClick(e)}
       dragBoundFunc={(pos) => {
         return {
           x: pos.x >= 111 ? pos.x : 111,
@@ -61,10 +94,17 @@ const Phrase = ({ id, trackIndex, layerData,phraseIndex }: any) => {
         id={"sub_layer_" + id}
         width={100}
         height={40}
-        fill={phraseData?.id === selectedPhrase?.id && selectedPhrase?.trackIndex === trackIndex ? "#4287f5" : "#BDBDBD"} 
+        fill={
+          phraseData?.id === selectedPhrase?.id &&
+          selectedPhrase?.trackIndex === trackIndex
+            ? "#4287f5"
+            : addRestriction() && !phraseData
+            ? "#ba7c77"
+            : "#BDBDBD"
+        }
         opacity={phraseData ? 0.8 : 0.2}
         stroke={"white"}
-        strokeWidth={1} 
+        strokeWidth={1}
         ref={shapeRef}
       />
       {layerData.phrase.toString().length > 30 ? (
