d2e069211f187967064fdb0eb256741bb047f627
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-04-02 12:02:35 +0530

    delete phrase validation

--- src/components/ContextMenu/ContextMenu.tsx
+++ src/components/ContextMenu/ContextMenu.tsx
@@ -9,43 +9,49 @@ import {
 import { ContextMenu } from "./styles";
 
 export default function ConetxtMenu() {
+  const contextMenu = useContextMenuState();
 
-  const contextMenu = useContextMenuState()
+  const editorState = useEditorState();
 
-  const editorState = useEditorState()
-
-  const selectedPhrase = useSelectedPhrase()
+  const selectedPhrase = useSelectedPhrase();
 
   const deleteSelectedPhrase = () => {
-   const updatedPhrases = editorState.phrases.filter((p)=> p !== selectedPhrase)
-   console.log(updatedPhrases)
-   setEditorState({...editorState, phrases:updatedPhrases})
-  }
-
-  const handleOptionClick = (action:string) => {
-    switch (action){
-      case 'delete':
-        console.log("delete")
-        deleteSelectedPhrase()
-      break;
-      case 'copy':
-        console.log("copy")
-      break;
-      case 'edit':
-        console.log("edit")
-      break;
+    if (selectedPhrase) {
+      const updatedPhrases = editorState.phrases.filter(
+        (p) => p !== selectedPhrase
+      );
+      console.log(updatedPhrases);
+      setEditorState({ ...editorState, phrases: updatedPhrases });
     }
-    setContextMenuState({open:false,event:null})
-  }
+  };
+
+  const handleOptionClick = (action: string) => {
+    switch (action) {
+      case "delete":
+        console.log("delete");
+        deleteSelectedPhrase();
+        break;
+      case "copy":
+        console.log("copy");
+        break;
+      case "edit":
+        console.log("edit");
+        break;
+    }
+    setContextMenuState({ open: false, event: null });
+  };
 
   return (
     <div>
       {contextMenu.open && (
-        <ContextMenu top={contextMenu.event?.evt.pageY} left={contextMenu.event?.evt.pageX}>
+        <ContextMenu
+          top={contextMenu.event?.evt.pageY}
+          left={contextMenu.event?.evt.pageX}
+        >
           <ul>
-            <li onClick={()=>handleOptionClick('edit')}>Edit</li>
-            <li onClick={()=>handleOptionClick('copy')}>Copy</li>
-            <li onClick={()=>handleOptionClick('delete')}>Delete</li>
+            <li onClick={() => handleOptionClick("edit")}>Edit</li>
+            <li onClick={() => handleOptionClick("copy")}>Copy</li>
+            <li onClick={() => handleOptionClick("delete")}>Delete</li>
           </ul>
         </ContextMenu>
       )}
